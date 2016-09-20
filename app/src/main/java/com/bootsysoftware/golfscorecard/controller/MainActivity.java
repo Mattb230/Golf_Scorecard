e4f40e62ef67ea55650e2ebd9281378ebd7b6aca
mattb230 <mattb230@gmail.com>
2016-09-19 19:45:51 -0500

    save stroke count via shared preferences

--- app/src/main/java/com/bootsysoftware/golfscorecard/controller/MainActivity.java
+++ app/src/main/java/com/bootsysoftware/golfscorecard/controller/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bootsysoftware.golfscorecard.controller;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,11 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends ListActivity {
 
+    private static final String PREFS_FILE = "com.bootsysoftware.golfscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "KEY_STROKECOUNT";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     //hold the hole objects
     private Hole[] mHoles = new Hole[18];
     //bind views
@@ -28,6 +35,10 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
         //initialize the array of Hole objects
         populateHoles();
         //Create and set the my ListView to use my custom adapter
@@ -38,10 +49,22 @@ public class MainActivity extends ListActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for(int i = 0; i < mHoles.length; i++){
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getNumStrokes());
+        }
+        mEditor.apply();
+    }
+
     private void populateHoles() {
+        int strokes = 0;
        // mHoles = new Hole[17];
         for(int i = 0; i < mHoles.length; i++){
-            mHoles[i] = new Hole(i+1, 0);
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
+            mHoles[i] = new Hole(i+1, strokes);
         }
 
     }
